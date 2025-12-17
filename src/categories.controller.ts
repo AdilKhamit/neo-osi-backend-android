9e73aaba483949993878a749dd8abba964bc6f77
Adil Khamit <101663828+AdilKhamit@users.noreply.github.com>
2025-12-17 17:09:40 +0500

    Add CategoriesController with findAll method

---
+++ src/categories.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('categories')
+export class CategoriesController {
+  @Get()
+  findAll() {
+    return [
+      {
+        id: 1,
+        title: 'Диспетчерское и аварийно-ремонтное обслуживание',
+        icon: 'PhoneInTalk'
+      },
+      {
+        id: 2,
+        title: 'Капитальный ремонт общего имущества',
+        icon: 'Build'
+      },
+      {
+        id: 3,
+        title: 'Сбор и вывоз ТБО и КГО',
+        icon: 'DeleteOutline'
+      },
+      {
+        id: 4,
+        title: 'Создание ОСИ',
+        icon: 'HomeWork'
+      }
+    ];
+  }
