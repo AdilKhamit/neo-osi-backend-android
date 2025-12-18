16a44796d29fc3409c0ee797fedf2bd475646b7b
Adil Khamit <101663828+AdilKhamit@users.noreply.github.com>
2025-12-18 22:17:30 +0500

    Enhance CreateUserDto with optional fullName field
    Added fullName field as optional to CreateUserDto.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
-// src/users/dto/create-user.dto.ts
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
-    email: string;
-    password: string;
-  }
+  @IsEmail({}, { message: 'Некорректный формат Email' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Пароль должен быть минимум 6 символов' })
+  password: string;
+
+  // 👇 ДОБАВЛЕНО ПОЛЕ ДЛЯ ИМЕНИ
+  @IsString()
+  @IsOptional() // Сделали необязательным, чтобы старый код не ломался
+  fullName?: string;
+}
